12074869f1b9ee71234f5dd9a70957ef01e6854b
zlm6666 <548541786@qq.com>
2026-06-20 01:32:24 +0000

    fix: 用_redirects重写/pub/friends到/api/links-qexo

--- functions/pub/friends.js
+++
@@ -1,7 +0,0 @@
-// functions/pub/friends.js
-// Qexo 兼容路径：/pub/friends → 路由到 /api/links-qexo
-import { onRequestGet as qexoGet, onRequestPost as qexoPost, onRequestOptions as qexoOptions } from '../api/links-qexo.js';
-
-export const onRequestGet = qexoGet;
-export const onRequestPost = qexoPost;
-export const onRequestOptions = qexoOptions;
